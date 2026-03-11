f9cc7146b59763fdd053b5367b7672e343692ae6
JulioCaue <juliocaue22@gmail.com>
2026-03-11 18:03:04 -0300

    comentarios adicionados para lembrar depois

--- 03_de_marco/main.c
+++ 03_de_marco/main.c
@@ -2,9 +2,12 @@
 #include <stdlib.h>
 #include <locale.h>
 #include <windows.h>
+
+// essas duas não funciona no linux :(
 #include <fcntl.h>
 #include <io.h>
 
+//função pra limpar a tela
 void limpar_tela(){
     #if defined(_WIN32) || defined (_WIN64)
         system("cls");
@@ -14,22 +17,29 @@ void limpar_tela(){
 }
 
 int main(){
-    system("chcp 65001 > nul");
+    system("chcp 65001 > nul"); //acho que isso troca o encoding do terminal (???)
     setlocale(LC_ALL, "pt_BR.UTF-8");
+
+    //daqui pra baixo é só tristeza
     SetConsoleOutputCP(CP_UTF8);
     SetConsoleCP(CP_UTF8);
 
-    wchar_t nome[50];
+
+    //aqui w = wide = dois bytes ao envés de um (aparentemente)
+    wchar_t nome[50]; 
     int idade;
     float altura;
 
+    //transforma em wide (eu acho???)
     _setmode(_fileno(stdin), _O_WTEXT);
 
+    //teste
     printf("á é í ó ú ç Á É Í Ó Ú Ç e ê Ê\n");
 
     printf("Digite seu nome: ");
     fgetws(nome,sizeof(nome),stdin);
 
+    //não sei oq esse L faz mas ele tá ai
     printf("Digite sua idade: ");
     wscanf(L"%d",&idade);
 
@@ -40,8 +50,10 @@ int main(){
 
     wprintf(L"\nSeu nome é: %lsSua idade é: %d\nSua altura é:%.2f",nome,idade,altura);
 
+    //aparentemente não funciona, mas tá aqui pra deixar janela aberta
     getchar();
     getchar();
 
     return 0;
 }
+
