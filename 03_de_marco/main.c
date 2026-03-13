148f30051f989cab06f89e422d3b8c4665001b0c
JulioCaue <juliocaue22@gmail.com>
2026-03-12 21:12:01 -0300

    Windows concertado.

--- 03_de_marco/main.c
+++ 03_de_marco/main.c
@@ -3,9 +3,15 @@
 #include <locale.h>
 #include <fcntl.h>
 
+#if defined(_WIN32) || defined (_WIN64)
+    #include <windows.h>
+    #include <io.h>
+#endif
+
+
 
 //função pra limpar a tela
-void limpar_tela(){
+void limpar_tela(void){
     #if defined(_WIN32) || defined (_WIN64)
         system("cls");
     #else
@@ -15,9 +21,7 @@ void limpar_tela(){
 
 //logica para windows (com variaveis wide)
 //só tristeza aqui como um todo, sinceramente
-void logica_windows(){
-    #include <windows.h>
-    #include <io.h>
+void logica_windows(void){
     system("chcp 65001 > nul"); //acho que isso troca o encoding do terminal (???)
     setlocale(LC_ALL, "pt_BR.UTF-8");
     SetConsoleOutputCP(CP_UTF8);
@@ -36,6 +40,7 @@ void logica_windows(){
 
     printf("Digite seu nome: ");
     fgetws(nome,sizeof(nome),stdin);
+    nome[wcscspn(nome,L"\n")]=L'\0';
 
     //não sei oq esse L faz mas ele tá ai
     printf("Digite sua idade: ");
@@ -46,14 +51,14 @@ void logica_windows(){
 
     limpar_tela();
 
-    wprintf(L"\nSeu nome é: %lsSua idade é: %d\nSua altura é:%.2f",nome,idade,altura);
+    wprintf(L"\nSeu nome é: %ls\nSua idade é: %d\nSua altura é:%.2f",nome,idade,altura);
 
     //aparentemente não funciona, mas tá aqui pra deixar janela aberta
     getchar();
     getchar();
 }
 
-void logica_linux(){
+void logica_linux(void){
     setlocale(LC_ALL, "pt_BR.UTF-8");
 
     char nome[50];
@@ -62,6 +67,7 @@ void logica_linux(){
 
     printf("Digite seu nome: ");
     fgets(nome,sizeof(nome),stdin);
+    nome[strcspn(nome,"\n")]='\0';
 
     printf("Digite sua idade: ");
     scanf("%d",&idade);
@@ -69,20 +75,22 @@ void logica_linux(){
     printf("Digite sua altura: ");
     scanf("%f",&altura);
 
-    void limpar_tela()
+    limpar_tela();
 
     printf("\nSeu nome: %sSua idade: %dSua altura: %.2f",nome,idade,altura);
 
     getchar();
     getchar();
-
 }
 
-#if defined(_WIN32) || defined (_WIN64)
-    void logica_windows();
-#else
-    void logica_linux();
-#endif
+void main(){
+    #if defined(_WIN32) || defined (_WIN64)
+        logica_windows();
+    #else
+        logica_linux();
+    #endif
+}
+
 
 
 //Tarefa: "informar nome, idade e altura. Depois informar esses dados pro usuário" 
